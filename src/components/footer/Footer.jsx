0816c8b4ab2319131d5d4c4a09a3b03d0ba036fa
Dinbandhu12 <dinbandhu8445@gmail.com>
2025-02-27 16:26:30 +0530

    added navbar code and components structures

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './favbar.module.css'
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>Footer</div>
+  )
+}
+
+export default Footer

08a3caee9bba42190e66251cb85d812e50a455d0
Dinbandhu12 <dinbandhu8445@gmail.com>
2025-03-06 14:10:06 +0530

    added theme toggle button and make navbar resposive

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import styles from './themeToggle.module.css'
+"use client";
+
+import React, { useContext } from "react";
+import styles from "./themeToggle.module.css";
+import Image from "next/image";
+import { ThemeContext } from "@/context/ThemeContext";
 
 const ThemeToggle = () => {
-  return (
-    <div>ThemeToggle</div>
-  )
-}
+  const { toggle, theme } = useContext(ThemeContext);
+  console.log(theme);
 
-export default ThemeToggle
+  return (
+    <div className={styles.container} onClick={toggle} style={
+      theme == "dark"
+        ? { backgroundColor: "white" }
+        : { backgroundColor: "#0f172a" }
+    }>
+      <Image src="/moon.png" alt="sun" width={14} height={14} />
+      <div
+        className={styles.ball}
+        style={
+          theme == "dark"
+            ? {left: 1, background: "#0f172a", transition: "all 0.5s ease-in-out" }
+            : {right: 1, background: "white", transition: "all 0.5s ease-in-out" }
+        }
+      ></div>
+      <Image src="/sun.png" alt="moon" width={14} height={14} />
+    </div>
+  );
+};
+
+export default ThemeToggle;
